92265339a32694813b7c1d6bbb694a44fa399b42
Hiranbaburaj <116332096+Hiranbaburaj@users.noreply.github.com>
2023-06-08 13:56:02 +0530

    Delete palindrome.sh

--- palindrome.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-read -p "Enter the word: " wd
-dw=$(echo $wd | rev)
-if [ "$wd" = "$dw" ]
-then echo "The Given Word is a Palindrome"
-else echo "The Given Word is not a Palindrome"
-fi
